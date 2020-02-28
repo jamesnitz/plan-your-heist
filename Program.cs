58036cd0befafd270c1533ea5d91999034d63b9b
jamesnitz <jamesanitz2@gmail.com>
2020-02-28 14:09:11 -0600

    phase 1 complete

--- Program.cs
+++ Program.cs
@@ -6,12 +6,20 @@ namespace plan_your_heist {
         static void Main (string[] args) {
             Console.WriteLine ("Plan Your Heist");
             Dictionary<int, string> teamMemberName = new Dictionary<int, string> ();
-            Dictionary<string, int> skillLevel = new Dictionary<string, int> ();
-            Dictionary<string, decimal> courageLevel = new Dictionary<string, decimal> ();
+            Dictionary<int, int> skillLevel = new Dictionary<int, int> ();
+            Dictionary<int, decimal> courageLevel = new Dictionary<int, decimal> ();
 
             Console.WriteLine ("Please enter team member Name!");
-
             teamMemberName.Add (teamMemberName.Count, Console.ReadLine ());
+            Console.WriteLine ("Please enter team member skill!");
+            skillLevel.Add (skillLevel.Count, int.Parse (Console.ReadLine ()));
+            Console.WriteLine ("Please enter team member courage!");
+            courageLevel.Add (courageLevel.Count, decimal.Parse (Console.ReadLine ()));
+            foreach (KeyValuePair<int, string> person in teamMemberName) {
+                int personSkillLevel = skillLevel[person.Key];
+                decimal personCourageLevel = courageLevel[person.Key];
+                Console.WriteLine ($"{person.Value} has skill level: {personSkillLevel} and courage level:  {personCourageLevel}");
+            }
 
         }
     }
