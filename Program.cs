c3101094335482513f06fb7c575945acfd0499a8
jamesnitz <jamesanitz2@gmail.com>
2020-02-28 15:12:09 -0600

    phase 2  done

--- Program.cs
+++ Program.cs
@@ -8,17 +8,22 @@ namespace plan_your_heist {
             Dictionary<int, string> teamMemberName = new Dictionary<int, string> ();
             Dictionary<int, int> skillLevel = new Dictionary<int, int> ();
             Dictionary<int, decimal> courageLevel = new Dictionary<int, decimal> ();
-
-            Console.WriteLine ("Please enter team member Name!");
-            teamMemberName.Add (teamMemberName.Count, Console.ReadLine ());
-            Console.WriteLine ("Please enter team member skill!");
-            skillLevel.Add (skillLevel.Count, int.Parse (Console.ReadLine ()));
-            Console.WriteLine ("Please enter team member courage!");
-            courageLevel.Add (courageLevel.Count, decimal.Parse (Console.ReadLine ()));
-            foreach (KeyValuePair<int, string> person in teamMemberName) {
-                int personSkillLevel = skillLevel[person.Key];
-                decimal personCourageLevel = courageLevel[person.Key];
-                Console.WriteLine ($"{person.Value} has skill level: {personSkillLevel} and courage level:  {personCourageLevel}");
+            while (true) {
+                Console.WriteLine ("Please enter team member Name!");
+                string singleMember = Console.ReadLine ();
+                if (singleMember == "") {
+                    foreach (KeyValuePair<int, string> person in teamMemberName) {
+                        int personSkillLevel = skillLevel[person.Key];
+                        decimal personCourageLevel = courageLevel[person.Key];
+                        Console.WriteLine ($"{person.Value} has skill level: {personSkillLevel} and courage level:  {personCourageLevel}");
+                    }
+                    break;
+                }
+                teamMemberName.Add (teamMemberName.Count, singleMember);
+                Console.WriteLine ("Please enter team member skill!");
+                skillLevel.Add (skillLevel.Count, int.Parse (Console.ReadLine ()));
+                Console.WriteLine ("Please enter team member courage!");
+                courageLevel.Add (courageLevel.Count, decimal.Parse (Console.ReadLine ()));
             }
 
         }
