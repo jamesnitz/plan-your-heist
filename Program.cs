af79a67c9f75afcc4dd9fe1f06d9719d20945e00
jamesnitz <jamesanitz2@gmail.com>
2020-02-28 13:40:33 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace plan_your_heist {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Plan Your Heist");
+            Dictionary<int, string> teamMemberName = new Dictionary<int, string> ();
+            Dictionary<string, int> skillLevel = new Dictionary<string, int> ();
+            Dictionary<string, decimal> courageLevel = new Dictionary<string, decimal> ();
+
+            Console.WriteLine ("Please enter team member Name!");
+
+            teamMemberName.Add (teamMemberName.Count, Console.ReadLine ());
+
+        }
+    }
+}
